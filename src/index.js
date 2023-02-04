0d462bffe4b5d1d2b04883b2a3dd0e45e9f8b774
Evilylla <107634746+Evilylla@users.noreply.github.com>
2023-02-04 20:44:32 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+const path = require('path');
+
 const app = require('express')();
 const http = require('http').Server(app);
 
 const PORT = 3000;
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
  
 http.listen(PORT, function(){
-    console.log('HTTP server started on port ' + PORT);
+    console.log('HTTP server started on port ' + PORT + __dirname);
 });
